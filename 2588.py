69eab878cc72071f2210fe5f4716351fd40930be
Changhwan Kim <kimch061279@gmail.com>
2023-01-17 04:36:20 +0000

    ;;

--- 2588.py
+++ 2588.py
@@ -1,11 +1,6 @@
 a = int(input(""))
 b = int(input(""))
 c = []
-d = []
-e = []
-f = []
-g = []
-
 for i in map(int, str(b)):
     c.append(i)
 
@@ -13,13 +8,6 @@ three = a*int(c[2])
 four = a*int(c[1])
 five = a*int(c[0])
 six = a*b
-
-for i in map(int, str(three)):
-    d.append(i)
-for i in map(int, str(four)):
-    e.append(i)
-for i in map(int, str(five)):
-    f.append(i)
 
 print(three)
 print(four)
