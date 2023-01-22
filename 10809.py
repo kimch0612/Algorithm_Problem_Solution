cee58b9c3291d556855e3441c9ac698abaaf6cd5
Changhwan Kim <kimch061279@gmail.com>
2023-01-22 13:35:10 +0000

    a

---
+++ 10809.py
@@ -0,0 +1,5 @@
+a = list(map(str, input()))
+alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+for i in range(0, len(a)):
+    if a[i] != alphabet[1]:
+        
